7a3bbbc6d700d13775bbc7f85e5ff05dff245cf2
shippc <wilhanmax@hotmail.com>
2022-08-10 22:33:59 -0300

    Javascript

---
+++ js/main.js
@@ -0,0 +1,11 @@
+(function () {
+    var $body = document.querySelector('body');
+    $body.classList.remove('no-js')
+    $body.classList.add('js')
+
+    var menu = new Menu ({
+        container: '.header-nav',
+        toggleBtn: '.header-btn-menu',
+        widthEnabled: 1024
+    })
+})()
