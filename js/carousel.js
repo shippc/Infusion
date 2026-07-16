559e85f8b84cea975a08dd9e9d8bcaf5fc3f9c58
shippc <wilhanmax@hotmail.com>
2026-07-16 14:27:26 -0300

    Refactor: redesign Insuion hero section

--- js/carousel.js
+++ js/carousel.js
@@ -1,53 +1,103 @@
 function Carousel(config) {
-    this.container = (typeof config.container === 'string') ? document.querySelector(config.container) : config.container
+    this.container = typeof config.container === "string"
+        ? document.querySelector(config.container)
+        : config.container;
 
-    this.itens = (typeof config.itens === 'string') ? this.container.querySelectorAll(config.itens) : config.itens
-
-    this.btnPrev = (typeof config.btnPrev === 'string') ? this.container.querySelector(config.btnPrev) : config.btnPrev
-
-    this.btnNext = (typeof config.btnNext === 'string') ? this.container.querySelector(config.btnNext) : config.btnNext
-
-    var _this = this;
-    var _currentSlide = 0
-
-    init()
-
-    function init() {
-        var _show = _this.container.querySelectorAll('.show')
-
-        Array.prototype.forEach.call(_show, function (sh) {
-            sh.classList.remove('show')
-        })
-        _this.itens[0].classList.add('show')
-        _this.btnNext.removeAttribute('style')
-        _this.btnPrev.removeAttribute('style')
-
-        addListeners()
+    if (!this.container) {
+        return;
     }
 
-    function addListeners() {
-        _this.btnNext.addEventListener('click', showNextSlide)
-        _this.btnPrev.addEventListener('click', showPrevSlide)
+    this.items = typeof config.items === "string"
+        ? this.container.querySelectorAll(config.items)
+        : config.items;
+
+    this.previousButton = typeof config.previousButton === "string"
+        ? this.container.querySelector(config.previousButton)
+        : config.previousButton;
+
+    this.nextButton = typeof config.nextButton === "string"
+        ? this.container.querySelector(config.nextButton)
+        : config.nextButton;
+
+    this.currentIndex = 0;
+
+    if (!this.items || this.items.length === 0) {
+        return;
     }
 
-    function showNextSlide() {
-        _currentSlide++;
-        showSlide()
-    }
+    this.showPreviousSlide = this.showPreviousSlide.bind(this);
+    this.showNextSlide = this.showNextSlide.bind(this);
+    this.handleKeyboard = this.handleKeyboard.bind(this);
 
-    function showPrevSlide() {
-        _currentSlide--;
-        showSlide()
-    }
-
-    function showSlide() {
-        var qtd = _this.itens.length;
-        var slide = _currentSlide % qtd;
-        slide = Math.abs(slide);
-
-        _this.container.querySelector('.show').classList.remove('show');
-        _this.itens[slide].classList.add('show')
-
-    }
-
+    this.init();
 }
+
+Carousel.prototype.init = function () {
+    var self = this;
+    var hasMultipleSlides = this.items.length > 1;
+
+    this.container.setAttribute("tabindex", "0");
+    this.container.setAttribute("aria-live", "polite");
+
+    Array.prototype.forEach.call(this.items, function (item, index) {
+        item.classList.toggle("show", index === 0);
+        item.setAttribute("aria-hidden", index === 0 ? "false" : "true");
+    });
+
+    if (this.previousButton) {
+        this.previousButton.removeAttribute("style");
+        this.previousButton.hidden = !hasMultipleSlides;
+        this.previousButton.addEventListener("click", this.showPreviousSlide);
+    }
+
+    if (this.nextButton) {
+        this.nextButton.removeAttribute("style");
+        this.nextButton.hidden = !hasMultipleSlides;
+        this.nextButton.addEventListener("click", this.showNextSlide);
+    }
+
+    this.container.addEventListener("keydown", this.handleKeyboard);
+
+    if (!hasMultipleSlides) {
+        self.container.removeAttribute("tabindex");
+    }
+};
+
+Carousel.prototype.showSlide = function (index) {
+    var totalItems = this.items.length;
+
+    this.currentIndex = (index + totalItems) % totalItems;
+
+    Array.prototype.forEach.call(
+        this.items,
+        function (item, itemIndex) {
+            var isCurrentSlide = itemIndex === this.currentIndex;
+
+            item.classList.toggle("show", isCurrentSlide);
+            item.setAttribute(
+                "aria-hidden",
+                isCurrentSlide ? "false" : "true"
+            );
+        }.bind(this)
+    );
+};
+
+Carousel.prototype.showPreviousSlide = function () {
+    this.showSlide(this.currentIndex - 1);
+};
+
+Carousel.prototype.showNextSlide = function () {
+    this.showSlide(this.currentIndex + 1);
+};
+
+Carousel.prototype.handleKeyboard = function (event) {
+    if (event.key === "ArrowLeft") {
+        event.preventDefault();
+        this.showPreviousSlide();
+    }
+
+    if (event.key === "ArrowRight") {
+        event.preventDefault();
+        this.showNextSlide();
+    }
+};
